35df5fa520f682ccb637d3135323aaa762abadf3
Maya <foenx0315@gmail.com>
2023-09-06 15:18:39 +0530

    Feat(Add); Docs

---
+++ src/lib/Footer.svelte
@@ -0,0 +1,8 @@
+<script>
+	import logo from './svelte-logo.svg';
+</script>
+
+<div class="max-w-4xl mx-auto w-full flex items-center justify-center" style="margin-top: 570px; margin-bottom: 0px;">
+  <img alt="Svelte logo" src={logo} class="w-6 h-6 mr-2" />
+  <div>Built with SvelteKit</div>
+</div>
